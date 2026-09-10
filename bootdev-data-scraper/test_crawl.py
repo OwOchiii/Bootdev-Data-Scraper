a66cd23eefaa13cc26bd5019cc6edc235faf83c8
Nguyen Vu An <nguyenvuan2005@gmail.com>
2026-09-10 23:04:55 +0700

    Update test_crawl.py

--- bootdev-data-scraper/test_crawl.py
+++ bootdev-data-scraper/test_crawl.py
@@ -26,6 +26,12 @@ class TestCrawl(unittest.TestCase):
         expected = "Main paragraph."
         self.assertEqual(actual, expected)
 
+    def test_get_urls_from_html_absolute(self):
+        input_url = "https://crawler-test.com"
+        input_body = '<html><body><a href="https://crawler-test.com"><span>Boot.dev</span></a></body></html>'
+        actual = get_urls_from_html(input_body, input_url)
+        expected = ["https://crawler-test.com"]
+        self.assertEqual(actual, expected)
 
 if __name__ == "__main__":
     unittest.main()
