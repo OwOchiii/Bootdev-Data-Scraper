ce65ab93e59c39358c40adb6e99a7206168697f5
Nguyen Vu An <nguyenvuan2005@gmail.com>
2026-09-15 17:55:16 +0700

    Update main.py

--- bootdev-data-scraper/main.py
+++ bootdev-data-scraper/main.py
@@ -1,5 +1,6 @@
 import sys
-from crawl import get_html
+from crawl import get_html, crawl_page
+
 
 def main():
     if len(sys.argv) > 2:
@@ -10,8 +11,9 @@ def main():
         sys.exit(1)
     url = sys.argv[1]
     print("starting crawl of:" + url)
-    input_body = get_html(url)
-    print(input_body)
+    page_data = crawl_page(url)
+    for url, data in page_data.items():
+        print(f"URL: {url}, Heading: {data['heading']}, First Paragraph: {data['first_paragraph']}")
 
 
 if __name__ == "__main__":
