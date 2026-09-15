462e6274b71f6e262a0823f0f027a25ae1c79ee6
Nguyen Vu An <nguyenvuan2005@gmail.com>
2026-09-15 17:37:48 +0700

    Update main.py

--- bootdev-data-scraper/main.py
+++ bootdev-data-scraper/main.py
@@ -1,5 +1,14 @@
+import sys
+
 def main():
-    print("Hello from bootdev-data-scraper!")
+    if len(sys.argv) > 2:
+        print("too many arguments provided")
+        sys.exit(1)
+    if len(sys.argv) < 2:
+        print("no website provided")
+        sys.exit(1)
+    url = sys.argv[1]
+    print("starting crawl of:" + url)
 
 
 if __name__ == "__main__":
